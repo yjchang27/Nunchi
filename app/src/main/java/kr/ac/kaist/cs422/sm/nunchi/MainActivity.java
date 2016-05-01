44361a0065b79727fcbec26a7b991b5121b04820
Youngjae Chang <caspian0207@gmail.com>
2016-05-01 21:52:11 +0900

    Add AndroidAnnotation, MainActivity, Service skeleton

---
+++ app/src/main/java/kr/ac/kaist/cs422/sm/nunchi/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kaist.cs422.sm.nunchi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/kr/ac/kaist/cs422/sm/nunchi/service/MyNotificationListenerService.java
@@ -0,0 +1,38 @@
+package kr.ac.kaist.cs422.sm.nunchi.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class MyNotificationListenerService extends NotificationListenerService {
+    public MyNotificationListenerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn, RankingMap rankingMap) {
+        super.onNotificationPosted(sbn, rankingMap);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        super.onNotificationRemoved(sbn);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn, RankingMap rankingMap) {
+        super.onNotificationRemoved(sbn, rankingMap);
+    }
+}
